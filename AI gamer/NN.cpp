1a5bbc3494250a847316ba50e4236d6778ad596b
CommanderLake <admin@commanderlake.net>
2025-10-23 01:56:30 +0100

    changes

--- AI gamer/NN.cpp
+++ AI gamer/NN.cpp
@@ -35,7 +35,7 @@ NN::NN(cudnnHandle_t cudnnHandle, cublasHandle_t cublasHandle, int w, int h, boo
 	constexpr auto embedDim = embedSqrt*embedSqrt;
 	constexpr auto ffDim = embedDim*4;
 	constexpr int numHeads = 8;
-	constexpr int numEncoders = 4;
+	constexpr int numEncoders = 8;
 	const int patchRows = DivCeil(netHeight, patchSize);
 	const int patchCols = DivCeil(netWidth, patchSize);
 	const auto nTokens = patchRows*patchCols;
