ac8865bf27d0a22f8998a07934df8e12334ba500
CommanderLake <admin@commanderlake.net>
2025-11-01 14:21:11 +0000

    changes

--- AI gamer/SpatialActionHead.cpp
+++ AI gamer/SpatialActionHead.cpp
@@ -27,10 +27,6 @@ SpatialActionHead::SpatialActionHead(const cudnnHandle_t cudnnHandle, const cubl
 	sharedLayers_.push_back(new BatchNorm(cudnn_, CUDNN_BATCHNORM_SPATIAL, batchSize_, trunkC_, sharedH, sharedW, "Spatial BN 1", train_, gradAccumLength_));
 	sharedLayers_.push_back(new GELULayer(batchSize_, trunkC_, sharedH, sharedW, "Spatial GELU 1"));
 	sharedLayers_.push_back(new Dropout(cudnn_, 0.2f, batchSize_, trunkC_, sharedH, sharedW, "Spatial Drop 1", train_));
-	sharedLayers_.push_back(new ConvLayer(cudnn_, batchSize_, trunkC_, trunkC_, 3, 1, 1, &sharedH, &sharedW, 1, "Spatial Conv 2", train_, weightDecay_, gradAccumLength_, Xavier));
-	sharedLayers_.push_back(new BatchNorm(cudnn_, CUDNN_BATCHNORM_SPATIAL, batchSize_, trunkC_, sharedH, sharedW, "Spatial BN 2", train_, gradAccumLength_));
-	sharedLayers_.push_back(new GELULayer(batchSize_, trunkC_, sharedH, sharedW, "Spatial GELU 2"));
-	sharedLayers_.push_back(new Dropout(cudnn_, 0.2f, batchSize_, trunkC_, sharedH, sharedW, "Spatial Drop 2", train_));
 	sharedHeight_ = sharedH;
 	sharedWidth_ = sharedW;
 	sharedOutC_ = trunkC_*sharedHeight_*sharedWidth_;
@@ -41,6 +37,8 @@ SpatialActionHead::SpatialActionHead(const cudnnHandle_t cudnnHandle, const cubl
 	sharedLayers_.push_back(new LayerNorm(batchSize_, outC, 1, 1, "Spatial-neck LN", train_));
 	sharedLayers_.push_back(new GELULayer(batchSize_, outC, 1, 1, "Spatial-neck GELU"));
 	sharedLayers_.push_back(new Dropout(cudnn_, 0.2f, batchSize_, outC, 1, 1, "Spatial-neck Drop", train_));
+	buttonLayers_.push_back(new FCLayer(cublas_, batchSize_, outC, outC, "Buttons FC", train_, weightDecay_, gradAccumLength_, Xavier, true));
+	buttonLayers_.push_back(new GELULayer(batchSize_, outC, 1, 1, "Buttons GELU"));
 	buttonLayers_.push_back(new FCLayer(cublas_, batchSize_, outC, NUM_BUTS_, "Buttons FC", train_, weightDecay_, gradAccumLength_, Xavier, true));
 	axisLayers_.push_back(new FCLayer(cublas_, batchSize_, outC, outC, "Axes FC 1", train_, weightDecay_, gradAccumLength_, Xavier, true));
 	axisLayers_.push_back(new GELULayer(batchSize_, outC, 1, 1, "Axes GELU"));
