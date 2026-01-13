2dd71e6dc50acc412091400a93d1c23ad995a0f6
CommanderLake <admin@commanderlake.net>
2026-01-13 22:13:07 +0000

    changes

--- AI gamer/Infer.cpp
+++ AI gamer/Infer.cpp
@@ -107,8 +107,8 @@ void Infer::ProcessOutput(const float* predictions){
 		inputs[inputIndex].mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
 		inputIndex++;
 	}
-	const int mouseX = static_cast<int>(std::sinh(predictions[14])*AXIS_SCALE_);
-	const int mouseY = static_cast<int>(std::sinh(predictions[15])*AXIS_SCALE_);
+	const int mouseX = static_cast<int>(std::sinh(predictions[NUM_BUTS_])*AXIS_SCALE_);
+	const int mouseY = static_cast<int>(std::sinh(predictions[NUM_BUTS_ + 1])*AXIS_SCALE_);
 	if(mouseX != 0 || mouseY != 0){
 		inputs[inputIndex].type = INPUT_MOUSE;
 		inputs[inputIndex].mi.dx = mouseX;
